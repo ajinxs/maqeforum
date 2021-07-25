e86f0ad450954c27f3dbbf736245fd0f2b5a5184
ajinxs <leeajink@gmail.com>
2021-07-25 16:24:40 +0700

    created css folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './css/App.css'
 import Header from './components/Header'
 import Location from './components/Location';
 
 function App() {
   return (
-    <div>
+    <div className='app'>
 
       <Header/>
       <Location/>
